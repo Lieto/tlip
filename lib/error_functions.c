bca988baa5cffdb1218caa3c55c27787990f3e5c
Vesa Kuoppala <vesa@parkkisahko.fi>
2015-12-26 10:38:20 +0200

    initial commit

---
+++ lib/error_functions.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include "error_functions.h"
+#include "tlpi_hdr.h"
+#include "ename.c.inc"
+
+#ifdef __GNUC__
+__attribute__ ((__noreturn__))
+#endif
+
+static void terminate(Boolean useExit3)
+{
+    char *s;
+
+    /* Dump core if EF_DUMPCORE environment variable is defined and
+       is a nonempty string; otherwise call exit(3) or _exit(2),
+       depending on the value of 'useExit3'. */
+    
+    s = getenv("EF_DUMPCORE");
+
+    if (s != NULL && *s != '\o')
+        abort();
+    else if (useExit3)
+        exit(EXIT_FAILURE);
+    else
+        _exit(EXIT_FAILURE);
+}
